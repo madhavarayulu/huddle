a8f785244ec75f7eb5e975060d4da48e4185a8c9
Madhava Rayulu <100406503+madhavarayulu@users.noreply.github.com>
2024-06-15 14:06:01 +0530

    Update Card.jsx
    modified asset path

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -10,7 +10,7 @@ export default function Card({item: { id, title, body, image } }) {
       </div>
 
       <div>
-        <img src={`/images/${image}`} alt="" />
+        <img src={`/${image}`} alt="" />
       </div>
     </StyledCard>
   )
